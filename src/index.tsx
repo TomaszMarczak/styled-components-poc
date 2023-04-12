683755e6aaa593cd7fdcc446eb6db02314347e5b
Tomasz Marczak <tommy.marczak@gmail.com>
2023-04-12 22:37:08 +0200

    Introduce Context API

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./ThemeContext";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
