0b334a1f5f776fd16dfa2a9ffae0987683ffe9a8
Tomasz Marczak <tommy.marczak@gmail.com>
2023-04-14 18:11:16 +0200

    Working application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState, ReactElement } from "react";
+import { useState, ReactElement, useEffect } from "react";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, randomTheme } from "./theme";
+import { lightTheme, darkTheme, randomLightColor } from "./theme";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.colors.text};
+`;
+const SubTitle = styled.h2`
+  color: ${(props) => props.theme.colors.text};
+`;
 
 const Container = styled.div`
   width: 100vw;
   height: 100%;
   min-height: 100vh;
   padding: 1rem;
-  background-color: #333;
+  background-color: ${(props) => props.theme.colors.bg};
   display: flex;
   flex-direction: column;
 `;
@@ -20,15 +27,17 @@ const TestContainer = styled.div`
 `;
 
 const ControlButton = styled.button`
-  color: black;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.bg};
   font-weight: bold;
   padding: 0.5rem 1rem;
   border: 3px solid #333;
+  cursor: pointer;
 `;
 
 const TestButton = styled.button`
-  background-color: white;
-  color: red;
+  background-color: ${(props) => props.theme.colors.bgButton};
+  color: ${(props) => props.theme.colors.text};
   font-weight: bold;
   width: 150px;
   padding: 0.5rem 1rem;
@@ -42,6 +51,33 @@ export default function App() {
   );
   const [theme, setTheme] = useState(() => "light");
 
+  const themeSet = () => {
+    switch (theme) {
+      case "light":
+        return lightTheme;
+      case "dark":
+        return darkTheme;
+      case "random":
+        return {
+          colors: {
+            bg: randomLightColor(),
+            text: "#333",
+            bgButton: randomLightColor(),
+          },
+        };
+      default:
+        return lightTheme;
+    }
+  };
+
+  useEffect(() => {
+    setComponents(generateComponents());
+  }, [quantity]);
+
+  const handleSetTheme = (input: string) => {
+    setTheme(input);
+  };
+
   function generateComponents() {
     const newComponents = [];
     for (let i = 0; i < quantity; i++) {
@@ -51,13 +87,27 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themeSet}>
       <Container>
-        <h1>styled-components theming</h1>
-        <div>
-          <ControlButton>Randomize theme</ControlButton>
+        <Title>styled-components theming</Title>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <ControlButton onClick={() => handleSetTheme("random")}>
+            Randomize theme
+          </ControlButton>
+          <ControlButton onClick={() => handleSetTheme("light")}>
+            Light theme
+          </ControlButton>
+          <ControlButton onClick={() => handleSetTheme("dark")}>
+            Dark theme
+          </ControlButton>
+          <input
+            type="number"
+            value={quantity}
+            min={0}
+            onChange={(e) => setQuantity(+e.target.value)}
+          ></input>
         </div>
-        <h2>Components</h2>
+        <SubTitle>Components</SubTitle>
         <TestContainer>{components}</TestContainer>
       </Container>
     </ThemeProvider>
--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-const randomLightColor = (): string => {
+export const randomLightColor = (): string => {
   //Generate 3 values between 200 and 255 and return them as a rgb value string
   const values = Array.from({ length: 3 }, () =>
     Math.floor(Math.random() * 55 + 200)
@@ -8,7 +8,8 @@ const randomLightColor = (): string => {
 
 export const lightTheme = {
   colors: {
-    bg: "#fff",
+    bg: "#ffff",
+    bgButton: "lightblue",
     text: "#000",
   },
 };
@@ -17,12 +18,8 @@ export const darkTheme = {
   colors: {
     bg: "#000",
     text: "#fff",
+    bgButton: "grey",
   },
 };
 
-export const randomTheme = {
-  colors: {
-    bg: randomLightColor(),
-    text: "#000",
-  },
-};
+export const randomTheme = {};
